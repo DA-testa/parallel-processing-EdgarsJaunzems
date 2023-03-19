51819016e7a48992b3eaf32e2f87f00af6135639
EdgarsJaunzems <124889727+EdgarsJaunzems@users.noreply.github.com>
2023-03-19 11:07:28 +0000

    f

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ def parallel_processing(n, m, data):
     output = []
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
+    laiks = 0
+    s = 0
+    k = 0
+    lar = []
+    
+    for job in range(m):
+        output.append((k, laiks))
+        lar.append(laiks)
+        k += 1
+        if len(lar) >= n:
+            if len(lar) > n:
+                s += 1
+            laiks = lar[len(lar)-n] + runtimes[s]
 
     return output
 
@@ -15,15 +28,20 @@ def main():
     # m - job count
     n = 0
     m = 0
+    n = map(int, input().split())
+    m = map(int, input().split())
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
     data = []
+    data = (list(map(int, input().split())))
 
     # TODO: create the function
     result = parallel_processing(n,m,data)
     
     # TODO: print out the results, each pair in it's own line
+    for r1, r2 in result:
+        print(r1,r2)
 
 
 
